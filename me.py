21c0ae3b939ff98a33a56ec22b02815951385ab8
SHIVA <shivanandpatil55550@gmail.com>
2025-05-04 21:24:27 +0530

    taking the veriable DIVE

--- me.py
+++ me.py
@@ -3,7 +3,11 @@ take =int(input("eneter the number "))
 b=2
 print(take+b)
 print("this is multiple of those two number's ", take*b)
-print("this is devision ", take/b)
+div=take/b
+if take>0:
+    print("this is devision ", div)
+else:
+    print("your input is wrong you should enter the input value is greter then 0  ")
 print("new statment ")
 "yakopa"
 "ice cream"
